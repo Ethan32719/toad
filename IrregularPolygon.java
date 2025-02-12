d813c7583acef62fd406b897d789f1f8c57ac861
Ethan32719 <ee6517@pleasantonusd.net>
2025-02-12 21:50:48 +0000

    I have wrote most of the irregular polygon class, and just have to write the draw method

--- IrregularPolygon.java
+++ IrregularPolygon.java
@@ -1,42 +1,46 @@
 import java.awt.geom.*; // for Point2D.Double
 import java.util.ArrayList; // for ArrayList
-import java.util.concurrent.TimeUnit;
-
 import gpdraw.*; // for DrawingTool
 
-
 public class IrregularPolygon {
-    private ArrayList<Point2D.Double> myPolygon = new ArrayList<Point2D.Double>();
+    private ArrayList<Point2D.Double> myPolygon;
 
-    // constructor
-    public IrregularPolygon() {}
 
-    // public methods
-    public void add(Point2D.Double aPoint)
-    {
-        // TODO: Add a point to the IrregularPolygon.
+    public IrregularPolygon() {
+        myPolygon = new ArrayList<>();
     }
 
+    
+    public void add(Point2D.Double aPoint) {
+        myPolygon.add(aPoint);
+    }
+
+    
     public double perimeter() {
-        // TODO: Calculate the perimeter.
-        return 3.14;
-    }
-
-    public double area() {
-        // TODO: Calculate the area.
-        return 0.0;
-    }
-
-    public void draw()
-    {
-        // Wrap the DrawingTool in a try/catch to allow development without need for graphics.
-        try {
-            // TODO: Draw the polygon.
-            // Documents: https://pavao.org/compsci/gpdraw/html/gpdraw/DrawingTool.html
-            DrawingTool pen = new DrawingTool(new SketchPad(500, 500));
-            pen.move(50, 50);
-        } catch (java.awt.HeadlessException e) {
-            System.out.println("Exception: No graphics support available.");
+        if (myPolygon.size() < 2) return 0.0;
+        
+        double perimeter = 0.0;
+        for (int i = 0; i < myPolygon.size(); i++) {
+            Point2D.Double p1 = myPolygon.get(i);
+            Point2D.Double p2 = myPolygon.get((i + 1) % myPolygon.size()); // Wrap around
+            perimeter += p1.distance(p2);
         }
+        return perimeter;
     }
-}
+
+   
+    public double area() {
+        if (myPolygon.size() < 3) return 0.0;
+        
+        double sum1 = 0.0, sum2 = 0.0;
+        for (int i = 0; i < myPolygon.size(); i++) {
+            Point2D.Double p1 = myPolygon.get(i);
+            Point2D.Double p2 = myPolygon.get((i + 1) % myPolygon.size()); // Wrap around
+            sum1 += p1.x * p2.y;
+            sum2 += p1.y * p2.x;
+        }
+        return Math.abs(sum1 - sum2) / 2.0;
+    }
+
+   
+  
